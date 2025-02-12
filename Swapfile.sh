c6f9fba5d24e301a36a31b722942c5aebab481d0
Naz <47510666+nazverdee@users.noreply.github.com>
2025-02-12 00:15:27 -0500

    Create Swapfile.sh

---
+++ Swapfile.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Steps to Increase Swap Memory"
+
+# Step 1: Check the Current Swap File Size
+
+ls -lh
+
+# Step 2: Check Free Memory and Swap Space
+
+# Step 3: Create the Swap File
+
+sudo fallocate -l 2G /swapfile
+
+sudo ls -lh /swapfile
+
+# Step 4: Configure Permissions for the Swap File
+
+sudo chmod 600 /swapfile
+
+ls -lh /swapfile
+
+# Step 5: Convert the File to Swap
+
+echo "----------------------------------------"
+
+# Step 6: Make the Swap File Persistent 
+# For this step check the Modify_stab.vim file
+
+# Step 7: Notify the System of the New Swap File
+
+sudo swapon /swapfile
+
+# Step 8: Verify the Swap Space
+
+free -h
